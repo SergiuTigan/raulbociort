2fb0b27e87aa4f0370b71583a4c2be788d5c5a6f
Sergiu Tigan <sergiu.tigan@gmail.com>
2024-01-06 17:27:07 +0200

    App Init

---
+++ src/scripts/services.js
@@ -0,0 +1,38 @@
+export const servicesData = [
+  {
+    navigationLink: "../../../services/architecture",
+    title: "Architecture",
+    image: {
+      src: "/img/icons/arch.png",
+      alt: "Architecture Icon",
+    },
+    description: "Architecture is the art and science of designing and engineering large structures and buildings."
+  },
+  {
+    navigationLink: "../../../services/interior-design",
+    title: "Interior Design",
+    image: {
+      src: "/img/icons/intDes.png",
+      alt: "Interior Design Icon",
+    },
+    description: "Interior design is the art and science of enhancing the interior of a building to achieve a healthier and more aesthetically pleasing environment for the people using the space."
+  },
+  {
+    navigationLink: "../../../services/landscape-design",
+    title: "Landscape Design",
+    image: {
+      src: "/img/icons/landscape.png",
+      alt: "Landscape Design Icon",
+    },
+    description: "Landscape design is an independent profession and a design and art tradition, practised by landscape designers, combining nature and culture."
+  },
+  {
+    navigationLink: "../../../services/full-business-plan",
+    title: "Full Business Plan",
+    image: {
+      src: "/img/icons/business.png",
+      alt: "Urban Design Icon",
+    },
+    description: "Urban design is the process of designing and shaping the physical features of cities, towns, and villages and planning for the provision of municipal services to residents and visitors."
+  }
+]
